1bda1028e500c1e6db7f3df0dffaa2162b1c76fc
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-09-25 23:11:10 +0700

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0369a1",
+        dark: "#15181d",
+        darkgray: "#090909",
+        secondary: "#64748b",
+      },
+    },
   },
   plugins: [],
-}
-
+};
