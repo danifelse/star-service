99146c5a2fffbfe50eec0f4abc17ad1ebfd06924
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-09-25 23:33:27 +0700

    create darkmode slice

---
+++ src/redux/slice/DarkModeSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const localTheme = localStorage.getItem("darkMode");
+
+const initialState = {
+  darkMode: localTheme === "true" ? true : false,
+};
+
+export const darkModeSlice = createSlice({
+  name: "darkMode",
+  initialState,
+  reducers: {
+    toggleDarkMode: (state) => {
+      localStorage.setItem("darkMode", !state.darkMode);
+      state.darkMode = !state.darkMode;
+    },
+  },
+});
+
+export const { toggleDarkMode } = darkModeSlice.actions;
+
+export default darkModeSlice.reducer;
