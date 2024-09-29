9a955e3adf0dedb8506287ddfbdddbc7c2b50633
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-09-29 23:54:35 +0700

    create new section

---
+++ src/components/Fragments/Section4.jsx
@@ -0,0 +1,21 @@
+export default function Section4() {
+    return (
+        <section className="bg-white dark:bg-gray-900" id="section4">
+            <div className="container">
+                <div className="px-4 mx-auto">
+                    <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                        <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Star Service ada untukmu</h2>
+                            <p className="mb-4">Kami memberikan layanan terbaik dengan teknisi smartphone berpengalaman dan pemecah masalah yang handal. Kami dapat menangani berbagai jenis perbaikan, dari kerusakan fisik hingga masalah perangkat lunak yang kompleks. Apapun masalahnya—layar pecah, baterai rusak, atau perangkat lunak bermasalah.</p>
+                            <p>kami siap memberikan solusi terbaik dan cepat, selalu siap mengatasi tantangan terbesar untuk memastikan perangkat Anda kembali berfungsi optimal.</p>
+                        </div>
+                        <div className="grid grid-cols-2 gap-4 mt-8">
+                            <img className="w-full rounded-lg" src="./images/repair.jpeg" alt="office content 1" />
+                            <img className="mt-4 w-full lg:mt-10 rounded-lg" src="./images/repair2.jpg" alt="office content 2" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
